069d356cddc4682cadd6bfb13fbf002e8e771f6b
ufukcelen <ufuk_celen@me.com>
2021-08-04 05:16:00 +0300

    linked

---
+++ dataVc/SecondVC.swift
@@ -0,0 +1,25 @@
+//
+//  SecondVC.swift
+//  dataVc
+//
+//  Created by Ufuk Celen on 4.08.2021.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+   
+    @IBOutlet weak var nameTextFieldS: UILabel!
+    
+    let name = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameTextFieldS.text = name
+    }
+    
+
+}
--- dataVc/ViewController.swift
+++ dataVc/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: "to2ndVC", sender: nil)
+        
+        
+        
+        
+    }
+    
 }
 
